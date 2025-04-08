d960e02ee38c0aac8611a994a960a718d81d8f32
Alp Ergur <alperenali@gmail.com>
2025-04-07 19:12:53 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,40 +1,41 @@
 import numpy as np
 from scipy.spatial import ConvexHull
 
+
+
 def preprocess_support_set(A):
-    #TODO: Will need to figure out how to handle N vertices not only 2
     hull = ConvexHull(A)
-    vertex_index = np.argmin(A.sum(axis=1))
-    v = A[vertex_index]
-    facet_normals = []
-    for i, simplex in enumerate(hull.simplices):
-        if vertex_index in simplex:
-            eq = hull.equations[i]
-            normal = eq[:-1]
-            facet_normals.append(normal)
-    facet_normals = np.array(facet_normals)
-    selected_normals = []
-    for normal in facet_normals:
-        if len(selected_normals) == 0:
-            selected_normals.append(normal)
-        else:
-            candidate = np.vstack(selected_normals + [normal])
-            if np.linalg.matrix_rank(candidate) == len(selected_normals) + 1:
-                selected_normals.append(normal)
-        #Has to be N vertices not only 2
-        if len(selected_normals) == 2:
-            break
-    #TODO: Has to be N vertices not only 2
-    if len(selected_normals) < 2:
-        raise ValueError("Could not find 2 linearly independent facet normals.")
+    vertex_indices = hull.vertices
+    hull_points = A[hull.vertices]
+    affine_basis = hull_points - hull_points[0]
+    affine_dim = np.linalg.matrix_rank(affine_basis)
+
+
+    for i in vertex_indices:
+         pivot = A[i]
+         active_constraints = []
+         selected_normals = []
+         tolerance = 1e-10  # Adjust tolerance if needed
+         for j, equation in enumerate(hull.equations):
+            if abs(np.dot(equation[:-1], pivot) + equation[-1]) < tolerance:
+                active_constraints.append(equation[:-1])    
+         
+         dimconstraint = np.linalg.matrix_rank(active_constraints)   
+         if dimconstraint == affine_dim:
+             for normal in active_constraints:
+                 candidate = np.vstack(selected_normals + [normal])
+                 if np.linalg.matrix_rank(candidate) == len(selected_normals) + 1:
+                    selected_normals.append(normal)
+                 elif len(selected_normals) == affine_dim:
+                     break
     H = np.vstack(selected_normals)
     U = H
     B = (U @ A.T).T
-    v_transformed = U @ v
+    v_transformed = U @ pivot
     A_tilde = B - v_transformed
-    return A_tilde, U, v
+    return A_tilde, U, pivot
 
-def build_support_set_for_system(d=3):
+def build_support_set_for_system(d):
     A = np.array([
         [2*d, 0],
         [0, 2*d],
@@ -45,14 +46,14 @@ def build_support_set_for_system(d=3):
     ], dtype=float)
     return A
 
-def example_preprocessing(d=3):
-    A = build_support_set_for_system(d)
-    A_tilde, U, v = preprocess_support_set(A)
-    print("Original support set A (exponents):")
-    print(A)
-    print("\nChosen vertex v:")
-    print(v)
-    print("\nTransformation matrix U:")
-    print(U)
-    print("\nPreprocessed support set A_tilde:")
-    print(A_tilde)
+
+A = build_support_set_for_system(3)
+A_tilde, U, v = preprocess_support_set(A)
+print("Original support set A (exponents):")
+print(A)
+print("\nChosen vertex:")
+print(v)
+print("\nTransformation matrix U:")
+print(U)
+print("\nPreprocessed support set A_tilde:")
+print(A_tilde)
